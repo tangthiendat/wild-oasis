278d44dcb830dbc02fd36efc6bbfe4ed9195b687
Tang Thien Dat <thiendattang@gmail.com>
2024-03-08 15:33:38 +0700

    Build the App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-import styled from "styled-components";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import GlobalStyles from "./styles/GlobalStyles";
@@ -11,6 +10,7 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
     return (
@@ -18,13 +18,15 @@ function App() {
             <GlobalStyles />
             <BrowserRouter>
                 <Routes>
-                    <Route index element={<Navigate replace to="dashboard" />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-                    <Route path="bookings" element={<Bookings />} />
-                    <Route path="cabins" element={<Cabins />} />
-                    <Route path="users" element={<Users />} />
-                    <Route path="settings" element={<Settings />} />
-                    <Route path="account" element={<Account />} />
+                    <Route element={<AppLayout />}>
+                        <Route index element={<Navigate replace to="dashboard" />} />
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="bookings" element={<Bookings />} />
+                        <Route path="cabins" element={<Cabins />} />
+                        <Route path="users" element={<Users />} />
+                        <Route path="settings" element={<Settings />} />
+                        <Route path="account" element={<Account />} />
+                    </Route>
                     <Route path="login" element={<Login />} />
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
