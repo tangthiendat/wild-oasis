ea399f881e0ad9847ef3efa8591e010c427b856a
Tang Thien Dat <thiendattang@gmail.com>
2024-03-09 20:34:12 +0700

    Mutations Deleting a Cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -8,3 +8,10 @@ export async function getCabins() {
     }
     return data;
 }
+
+export async function deleteCabin(id) {
+    const { error } = await supabase.from("cabins").delete().eq("id", id);
+    if (error) {
+        throw new Error("Cabin could not be deleted");
+    }
+}
