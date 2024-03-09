2bc8dca659dea11ef6121e10cf4b7ecb89e8897f
Tang Thien Dat <thiendattang@gmail.com>
2024-03-09 09:56:34 +0700

    Connect to Supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://cazcsumbpijozdhlweyh.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhemNzdW1icGlqb3pkaGx3ZXloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4ODk0OTcsImV4cCI6MjAyNTQ2NTQ5N30.j4B_qjygyddOfh0I9HRNfONrRY2QMwAIGfiQF81W0ls";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
