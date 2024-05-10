e3a44e41d02ea2a0105eba1f99f0b3ca27c7e3c2
Tang Thien Dat <thiendattang@gmail.com>
2024-05-10 14:13:41 +0700

    Build a Reusable Table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,15 +4,7 @@ import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
-
-const Table = styled.div`
-    border: 1px solid var(--color-grey-200);
-
-    font-size: 1.4rem;
-    background-color: var(--color-grey-0);
-    border-radius: 7px;
-    overflow: hidden;
-`;
+import Table from "../../ui/Table";
 
 const TableHeader = styled.header`
     display: grid;
@@ -37,15 +29,15 @@ function CabinTable() {
     }
 
     return (
-        <Table role="table">
-            <TableHeader role="row">
+        <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+            <Table.Header role="row">
                 <div></div>
                 <div>Cabin</div>
                 <div>Capacity</div>
                 <div>Price</div>
                 <div>Discount</div>
                 <div></div>
-            </TableHeader>
+            </Table.Header>
             {cabins && cabins.map((cabin) => <CabinRow key={cabin.id} cabin={cabin} />)}
         </Table>
     );
