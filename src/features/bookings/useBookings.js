4e1aa7a559908677963d59cf84df83740f7b678b
Tang Thien Dat <thiendattang@gmail.com>
2024-05-13 19:54:05 +0700

    Prefetching with React Query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constant";
 
 export function useBookings() {
     const [searchParams] = useSearchParams();
+    const queryClient = useQueryClient();
 
     //1. FILTER
     const filterValue = searchParams.get("status");
@@ -21,5 +23,22 @@ export function useBookings() {
         queryKey: ["bookings", filter, sortBy, page],
         queryFn: () => getBookings({ filter, sortBy, page }),
     });
+
+    //4. PREFETCHING
+    const pageCount = Math.ceil(count / PAGE_SIZE);
+    if (page < pageCount) {
+        queryClient.prefetchQuery({
+            queryKey: ["bookings", filter, sortBy, page + 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+        });
+    }
+
+    if (page > 1) {
+        queryClient.prefetchQuery({
+            queryKey: ["bookings", filter, sortBy, page - 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+        });
+    }
+
     return { isLoading, bookings, count };
 }
