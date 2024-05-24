65b13bda5f871f3d1712fc03bc542869ccf3de5f
tangthiendat <thiendattang@gmail.com>
2024-05-24 15:00:11 +0700

    Build the Dashboard Layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,22 @@
+/* eslint-disable react/no-unescaped-entities */
 import styled from "styled-components";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+   display: grid;
+   grid-template-columns: 1fr 1fr 1fr 1fr;
+   grid-template-rows: auto 34rem auto;
+   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+   return (
+      <StyledDashboardLayout>
+         <div>Statistics</div>
+         <div>Today's activity</div>
+         <div>Chart stay duration</div>
+         <div>Chart sales</div>
+      </StyledDashboardLayout>
+   );
+}
+
+export default DashboardLayout;
